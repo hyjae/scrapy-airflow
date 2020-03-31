3a6bfde9c25e4081f371da8dc68c868b55d71935
Youngjae <hyjae@datasolution.kr>
2020-03-31 11:16:50 +0900

    update

--- scrapy_airflow/loader.py
+++ scrapy_airflow/loader.py
@@ -9,75 +9,89 @@ from scrapy.crawler import CrawlerProcess
 
 
 class SpiderLoader:
-    spiders = None
-
-    @classmethod
-    def get_spiders(cls):
-        config_loader = ConfigLoader()
-        spider_path = config_loader['spider_path']
-
-        curr_path = os.path.dirname(__file__)
-        
-        spider_dir = os.path.join(curr_path, 'spiders')
-        config_dir = os.path.join(curr_path, 'config.cfg')
-
-        cls.spiders = cls.module_loader(spider_dir)
-
-        for key, value in cls.spiders.items():
-            "By default, each spider dict has keys 'spider' and 'args'"
-            cls.spiders[key] = dict({'spider': cls.spiders[key]})
-            cls.spiders[key]['args'] = list()
-        for key, value in ConfigLoader().config.items():
-            if key in cls.spiders:
-                cls.spiders[key].update(cls.set_args())
-        return cls.spiders
 
     @staticmethod
     def get_spider(spider_name):
-        """This function get a spider module from the given directoy in scrapy-airflow.cfg
+        """This function get a spider module from the given directory in scrapy-airflow.cfg
 
         :param spider_name: spider script name or spider name
         :return: spider module object
         """
-
+        config_loader = ConfigLoader()
+        spider_root = config_loader['spider_path']
+        spider_path = os.path.join(spider_root, spider_name)
+        spider_module = SpiderLoader.module_loader(spider_path)
+        return spider_module
 
     @staticmethod
-    def module_loader(dir):
+    def module_loader(py_dir):
         """This finds a .py in a given directory;
         a multiple files with the same name would result in the first one found
 
-        :param dir: an absolute path directory where a module is located
+        :param py_dir: an absolute path directory where a module is located
         """
-        modules = dict()
-        package = 'scrapy.crawler.spiders'
+        importlib.import_module(py_dir)
+        # getattr(module)
 
-        config_loader = ConfigLoader()
-        config_loader.config
-
-        for file in glob.glob(os.path.join(dir, '*.py')):
-            name = os.path.splitext(os.path.basename(file))[0]
-            if name.startswith('_'):
-                continue
-            module = importlib.import_module('.'+name, package=package)
-            # member == classname
-            for member in dir(module):
-                "Return only classes that have the same name as filenames"
-                if member.lower() == name:
-                    handler_class = getattr(module, member)
-                    if handler_class and inspect.isclass(handler_class):
-                        modules[name] = handler_class
-        return modules
-
-
-class MySpider(scrapy.Spider):
-    # Your spider definition
-    ...
+        # package = 'scrapy.crawler.spiders'
+        #
+        # config_loader = ConfigLoader()
+        # config_loader.config
+        #
+        # for file in glob.glob(os.path.join(py_dir, '*.py')):
+        #     name = os.path.splitext(os.path.basename(file))[0]
+        #     if name.startswith('_'):
+        #         continue
+        #     module = importlib.import_module('.'+name, package=package)
+        #     # member == classname
+        #     for member in py_dir(module):
+        #         "Return only classes that have the same name as filenames"
+        #         if member.lower() == name:
+        #             handler_class = getattr(module, member)
+        #             if handler_class and inspect.isclass(handler_class):
+        #                 modules[name] = handler_class
+        # return modules
 
 
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'json',
-    'FEED_URI': 'items.json'
-})
+def runner(spider):
+    process = CrawlerProcess(settings={
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'items.json'
+    })
 
-process.crawl(MySpider)
-process.start()
+    process.crawl(spider)
+    process.start()
+
+import json
+
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy import log, signals
+from scrapy.utils.project import get_project_settings
+from scrapy_airflow.scrapy.crawler.spiders.us_exchange import *
+
+class MyPipeline(object):
+    def process_item(self, item, spider):
+        results.append(dict(item))
+
+results = []
+def spider_closed(spider):
+    print(results)
+
+# set up spider
+spider = USExchangeSpider()
+
+# set up settings
+settings = get_project_settings()
+settings.overrides['ITEM_PIPELINES'] = {'__main__.MyPipeline': 1}
+
+# set up crawler
+crawler = Crawler(settings)
+crawler.signals.connect(spider_closed, signal=signals.spider_closed)
+crawler.configure()
+crawler.crawl(spider)
+
+# start crawling
+crawler.start()
+log.start()
+reactor.run()
